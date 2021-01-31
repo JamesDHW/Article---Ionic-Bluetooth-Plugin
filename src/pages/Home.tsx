188fa869608f6feea6eadd513d5e97fe06ef5500
James <JamesHaworthWheatman@gmail.com>
2021-01-31 16:10:49 +0000

    Frontend app layout setup

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,23 +1,47 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import React from 'react';
-import ExploreContainer from '../components/ExploreContainer';
-import './Home.css';
+import {
+  IonContent,
+  IonHeader,
+  IonPage,
+  IonTitle,
+  IonToolbar,
+  IonInput,
+  IonItem,
+  IonFabButton,
+  IonIcon,
+} from "@ionic/react";
+import React, { useState } from "react";
+import "./Home.css";
+import { sendOutline } from "ionicons/icons";
 
 const Home: React.FC = () => {
+  const [message, setMessage] = useState("");
+  const [receivedMessage, setRecievedMessage] = useState("My message");
+  const onSend = () => {
+    setRecievedMessage(message);
+  };
+
   return (
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Blank</IonTitle>
+          <IonTitle>Nearby messages</IonTitle>
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Blank</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <ExploreContainer />
+        <div className="conatiner">
+          <IonTitle>{receivedMessage}</IonTitle>
+          <div className="message-bar">
+            <IonItem className="ion-item">
+              <IonInput
+                placeholder="Message to send"
+                onIonInput={(e: any) => setMessage(e.target.value)}
+              />
+            </IonItem>
+            <IonFabButton className="ion-fab" onClick={onSend}>
+              <IonIcon icon={sendOutline} />
+            </IonFabButton>
+          </div>
+        </div>
       </IonContent>
     </IonPage>
   );
