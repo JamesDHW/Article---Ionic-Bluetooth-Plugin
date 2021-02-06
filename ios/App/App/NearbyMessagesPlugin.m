b53757d8faaa57d113b899d15ede1e9641703164
James <JamesHaworthWheatman@gmail.com>
2021-02-06 15:56:51 +0000

    add and register ios plugin

---
+++ ios/App/App/NearbyMessagesPlugin.m
@@ -0,0 +1,12 @@
+//
+//  NearbyMessagesPlugin.m
+//  App
+//
+//  Created by James Haworth Wheatman on 31/01/2021.
+//
+
+#import <Capacitor/Capacitor.h>
+
+CAP_PLUGIN(NearbyMessagesPlugin, "NearbyMessagesPlugin",
+  CAP_PLUGIN_METHOD(HelloWorld, CAPPluginReturnPromise);
+)
